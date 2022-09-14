1257b1ff1c0764f1b9f74b1d5ea72bf45acae44d
Konrad Dawydiuk <kd250241@ncr.com>
2022-09-14 08:27:50 +0200

    Initial commit

---
+++ src/main/java/pl/home/helper/HhHomeHelperApplication.java
@@ -0,0 +1,13 @@
+package pl.home.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhHomeHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhHomeHelperApplication.class, args);
+	}
+
+}
